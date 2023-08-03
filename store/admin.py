8c6f2766ac35355345413f40c04691658d8deda0
ACE-47 <haidermohammed66233@gmail.com>
2023-08-03 11:00:14 +0300

    init

--- store/admin.py
+++ store/admin.py
@@ -1,13 +1,56 @@
+from typing import Any, List, Optional, Tuple
 from django.contrib import admin
+from django.db.models.query import QuerySet
+from django.http.request import HttpRequest
+from django.db.models import Count
+from django.utils.html import format_html, urlencode
+from django.urls import reverse
+
 from . import models
 
 # Register your models here.
 
+class InventoryFilter(admin.SimpleListFilter):
+    title = 'Inventory'
+    parameter_name = 'inventory'
+
+    def lookups(self, request: Any, model_admin: Any) -> List[Tuple[Any, str]]:
+        return [
+            ('<10','LOW'),
+        ]
+    
+    def queryset(self, request: Any, queryset: QuerySet()) :
+        if self.value() == '<10':
+            return queryset.filter(inventory__lt = 10)
+        
+class ProductImageInline(admin.TabularInline):
+    model = models.ProductImage
+    readonly_fields = ['thumbnail']
+
+    def thumbnail(self, instanc : models.ProductImage):
+        return format_html(f'<img src="{instanc.image.url}" class="thumbnail"/>')
+
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
+    inlines = [ProductImageInline]
     search_fields =['title']
-    # autocomplete_fields = ['collection']
+    autocomplete_fields = ['collection']
     prepopulated_fields = {'slug':['title']}
     list_display = ['title', 'unit_price', 'collection', 'inventory']
-    list_per_page = 10
+    list_filter = ['collection','last_update', InventoryFilter]
 
+    list_per_page = 20
+
+
+@admin.register(models.Collection)
+class CollectionAdmin(admin.ModelAdmin):
+    search_fields = ['title']
+    list_display = ['title','products_count']
+
+    @admin.display(ordering='products_count')
+    def products_count(self, collection):
+        url = (reverse('admin:store_product_changelist') + '?' + urlencode({'collection_id':str(collection.id)}))
+        return format_html('<a href={}>{}<a>',url,collection.products_count)
+
+    def get_queryset(self, request: HttpRequest) -> QuerySet[Any]:
+        return super().get_queryset(request).annotate(products_count = Count('products'))
