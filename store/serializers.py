f83a4ba270836a6644f99a407963c6e8a1773375
ACE-47 <haidermohammed66233@gmail.com>
2023-08-05 11:05:54 +0300

    init

--- store/serializers.py
+++ store/serializers.py
@@ -1,7 +1,17 @@
 from rest_framework import serializers
 from . import models
 
+
+class CollectionSerializer(serializers.ModelSerializer):
+    products_count = serializers.IntegerField(read_only = True)
+    class Meta:
+        model = models.Collection
+        fields = ['id','title', 'products_count']
+
+    
+
 class ProductImageSerializer(serializers.ModelSerializer):
+    
     class Meta:
         model = models.ProductImage
         fields = ['id', 'image']
@@ -17,3 +27,10 @@ class ProdcutSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Product
         fields = ['id','title','description','slug','inventory','unit_price','collection','images']
+
+
+
+class CartSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Cart
+        fields = ['id','']
