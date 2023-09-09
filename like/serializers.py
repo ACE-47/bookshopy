0c46cd2bc85b0c0ea76ad85331c75b9f05b190b9
ACE-47 <haidermohammed66233@gmail.com>
2023-09-09 12:55:35 +0300

    init

--- like/serializers.py
+++ like/serializers.py
@@ -1,9 +1,47 @@
 from rest_framework import serializers
 from .models import LikedItem
 from store.serializers import SimpleProductSerializer
+from store.models import Product
 
 class LikedItemSerializer(serializers.ModelSerializer):
-    productLiked = SimpleProductSerializer(many = True)
+    productLiked = SimpleProductSerializer()
     class Meta:
         model = LikedItem
-        fields = ['user','productLiked']
+        fields = ['productLiked']
+
+
+class AddLikedItemSerializer(serializers.ModelSerializer):
+    product_id = serializers.IntegerField()
+    class Meta:
+        model = LikedItem
+        fields = ['product_id']
+
+
+    def validate_product_id(self, value):
+        if not Product.objects.filter(pk = value).exists():
+            return serializers.ValidationError('No Product with the given ID was found!')
+
+        return value
+
+
+    def save(self, **kwargs):
+        user_id = self.context['user_id']
+        product_id = self.validated_data['product_id']
+
+        product = Product.objects.get(pk = product_id )
+        try:
+            likedItem = LikedItem.objects.prefetch_related('productLiked').get(user_id = user_id, productLiked__id= product_id)
+            print(likedItem)
+            
+            # likedItem.first().productLiked.clear()
+            # self.instance = likedItem
+        
+        except LikedItem.DoesNotExist :
+            
+            likedItem = LikedItem.objects.create(user_id = user_id)
+            print(likedItem.productLiked)
+            
+            self.instance = likedItem
+
+            print(self.instance)        
+        return self.instance
--- like/views.py
+++ like/views.py
@@ -1,16 +1,17 @@
 from django.shortcuts import render
+from requests import Response
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticated
-from .serializers import LikedItemSerializer
+from .serializers import LikedItemSerializer, AddLikedItemSerializer
 from .models import LikedItem
 # Create your views here.
 
 
 class LikedItemViewSet(ModelViewSet):
     # queryset = LikedItem.objects.all()
-   
+    
     serializer_class = LikedItemSerializer
-    http_method_names = ['get','patch','post','delete','head','options']
+    # http_method_names = ['get','patch','post','delete','head','options']
 
     
 
@@ -18,9 +19,29 @@ class LikedItemViewSet(ModelViewSet):
         user = self.request.user
 
         if user.is_staff :
-            return LikedItem.objects.prefetch_related('productLiked').all()
+            return LikedItem.objects.select_related('productLiked').all()
         
-        return LikedItem.objects.filter(user_id = user.id)
+        return LikedItem.objects.select_related('productLiked').filter(user_id = user.id)
     
+
+    def get_serializer_class(self):
+        if self.request.method == 'POST':
+            return AddLikedItemSerializer
+        
+        return LikedItemSerializer
+    
+    def get_serializer_context(self):
+        return {'user_id':self.request.user.id}
+    
+    
+    # def create(self, request, *args, **kwargs):
+    #     serializer = AddLikedItemSerializer.create(data = request.data, context= {'user_id':self.request.user.id})
+    #     serializer.is_valid(raise_exception=True)
+    #     itemLiked = serializer.save()
+    #     serializer = LikedItemSerializer(itemLiked)
+    #     return Response(serializer.data)
+    
+
+
 
     
